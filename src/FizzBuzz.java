0ae53d719053b9268526a2b2be411d13c3c514a9
Jack Lam <catinacan@gmail.com>
2015-02-04 13:42:57 -1000

    Answer to FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Jack on 2/4/2015.
+ */
+public class FizzBuzz {
+  public static void main(String[] args) {
+    for(int i =0; i< 101; i++){
+      System.out.println(intToString(i));
+    }
+  }
+
+  public static String intToString(int a){
+    String ret;
+    if((a%5 == 0) && (a%3==0)){
+      ret = "FizzBuzz";
+    }
+    else if(a%5==0){
+      ret = "Buzz";
+    }
+    else if(a%3==0){
+      ret = "Fizz";
+    }
+    else{
+      ret = Integer.toString(a);
+    }
+    return ret;
+  }
+}
